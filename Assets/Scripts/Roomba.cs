8110b910ae283bfaa9e71dc32ba4f34a8dbf2712
Aidan-2020 <69286745+Aidan-2020@users.noreply.github.com>
2025-03-15 17:26:28 -0400

    added basic enemy

---
+++ Assets/Scripts/Roomba.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Roomba : MonoBehaviour
+{
+    public float speed;
+    public float radius;
+    private Rigidbody2D EnemyRB;
+    public GameObject groundCheck;
+    public LayerMask groundLayer;
+    public bool facingRight;
+    public bool isGrounded;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        EnemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyRB.linearVelocity = Vector2.right * speed * Time.deltaTime;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, radius, groundLayer);
+        if(!isGrounded && facingRight)
+        {
+            Flip();
+        }
+        else if(!isGrounded && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(new Vector3(0, 180, 0));
+        speed = -speed;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.transform.position, radius);
+    }
+}
