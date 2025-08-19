3913a8a717dc956308f8a5606ebb65e7b40aa372
EvanMcRae <evanmmcrae@gmail.com>
2025-08-19 00:32:49 -0400

    Fix boxes floating when pushed off a cliff

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Box : MonoBehaviour
 {
     private Rigidbody2D rb;
-    [SerializeField] private float fallThreshold = -0.01f;
+    private bool grounded = false;
+    [SerializeField] private PhysicsMaterial2D slippery, friction;
 
     private void Start()
     {
@@ -12,14 +14,30 @@ public class Box : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Stop moving horizontally if falling
-        if (rb.linearVelocityY < fallThreshold)
+        // Stop moving horizontally if not on ground
+        grounded = false;
+        List<ContactPoint2D> contacts = new();
+        rb.GetContacts(contacts);
+        foreach (ContactPoint2D contact in contacts)
+        {
+            // TODO: Amend this check for other ground collidable objects
+            if (contact.collider.gameObject.layer == 6 || contact.collider.gameObject.layer == 10 || contact.collider.gameObject.layer == 11)
+            {
+                if (Mathf.Abs((contact.normal - Vector2.up).magnitude) <= 0.001f)
+                {
+                    grounded = true;
+                }
+            }
+        }
+        if (!grounded && rb.constraints != (RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation))
         {
             rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            rb.sharedMaterial = slippery;
         }
-        else
+        else if (grounded && rb.constraints != RigidbodyConstraints2D.FreezeRotation)
         {
             rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            rb.sharedMaterial = friction;
         }
     }
 }
