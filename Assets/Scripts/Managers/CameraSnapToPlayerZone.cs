82d4cd55caed7e6d9a78d227205a1b9fa5b62297
Jetinat0r <jetin8t0r@gmail.com>
2025-08-31 16:48:05 -0400

    Vent Travel

--- Assets/Scripts/Managers/CameraSnapToPlayerZone.cs
+++ Assets/Scripts/Managers/CameraSnapToPlayerZone.cs
@@ -7,7 +7,6 @@ using UnityEngine.UIElements;
 public class CameraSnapToPlayerZone : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private Vector2Int zoneSize = new Vector2Int(10, 9);
     private Vector2Int currentZone;
     [SerializeField] private float scrollTime = .75f;
     private Tween panTween = null;
@@ -17,12 +16,19 @@ public class CameraSnapToPlayerZone : MonoBehaviour
     {
         if (target == null)
         {
-            Debug.LogError("Error: Camera needs a target to follow");
+            //Debug.LogError("Error: Camera needs a target to follow");
         }
 
         //currentZone = new Vector2Int(Mathf.RoundToInt(target.position.x / zoneSize.x), Mathf.RoundToInt(target.position.y / zoneSize.y));
-        currentZone = new Vector2(MathUtils.GetClosestEvenDivisor(target.position.x, zoneSize.x) / zoneSize.x, MathUtils.GetClosestEvenDivisor(target.position.y, zoneSize.y) / zoneSize.y).ToVector2Int();
-        Vector2 _newPos = currentZone * zoneSize;
+        
+    }
+
+    public void Init(Transform _target)
+    {
+        target = _target;
+
+        currentZone = new Vector2(MathUtils.GetClosestEvenDivisor(target.position.x, LevelManager.instance.zoneSize.x) / LevelManager.instance.zoneSize.x, MathUtils.GetClosestEvenDivisor(target.position.y, LevelManager.instance.zoneSize.y) / LevelManager.instance.zoneSize.y).ToVector2Int();
+        Vector2 _newPos = currentZone * LevelManager.instance.zoneSize;
         transform.position = new Vector3(_newPos.x, _newPos.y, transform.position.z);
     }
 
@@ -31,13 +37,13 @@ public class CameraSnapToPlayerZone : MonoBehaviour
         if (LevelMenuManager.playerOverride) return;
 
         // Only move screen in distinct zones, snap to current zone (2D Zelda / Metroid)
-        Vector2Int _newZone = new Vector2(MathUtils.GetClosestEvenDivisor(target.position.x, zoneSize.x) / zoneSize.x, MathUtils.GetClosestEvenDivisor(target.position.y, zoneSize.y) / zoneSize.y).ToVector2Int();
+        Vector2Int _newZone = new Vector2(MathUtils.GetClosestEvenDivisor(target.position.x, LevelManager.instance.zoneSize.x) / LevelManager.instance.zoneSize.x, MathUtils.GetClosestEvenDivisor(target.position.y, LevelManager.instance.zoneSize.y) / LevelManager.instance.zoneSize.y).ToVector2Int();
         if (_newZone != currentZone)
         {
             currentZone = _newZone;
             panTween?.Kill();
             
-            Vector2 _newPos = _newZone * zoneSize;
+            Vector2 _newPos = _newZone * LevelManager.instance.zoneSize;
             
             oldTimeScale = Time.timeScale;
             Time.timeScale = 0f;
---
+++ Assets/Scripts/Managers/LevelGrid.cs
@@ -0,0 +1,157 @@
+using UnityEngine;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+//Thanks to kurtdekker on Github: https://gist.github.com/kurtdekker/079cb9fbc6d2ecf0a13ba045e96d23d2
+// @kurtdekker - ultra Cheesy grid with in-built editor for Unity3D
+//
+// To use:
+//	make an empty game object
+//	drag this script on it
+// 	make a prefab out of it
+//	select the prefab to edit the grid
+//
+// now you can cheesy-easy edit a smallish grid in the Unity editor window
+//
+public class LevelGrid : MonoBehaviour
+{
+    [Header("Cell values cycle through this list. MUST BE SINGLE CHARS!")]
+    public List<string> cellValues = new List<string> { "F", "T", "*" };
+
+    [Header("Actual saved payload. Use GetCell(x,y) to read!")]
+    [Header("WARNING: changing this will nuke your data!")]
+    public string data;
+
+    public int width = 4;
+    public int height = 4;
+
+    public string GetCell(int x, int y)
+    {
+        int n = GetIndex(x, y);
+        return data.Substring(n, 1);
+    }
+
+    public int GetIndex(int x, int y)
+    {
+        if (x < 0) return -1;
+        if (y < 0) return -1;
+        if (x >= width) return -1;
+        if (y >= height) return -1;
+        return x + y * width;
+    }
+
+    public void ToggleCell(int x, int y)
+    {
+        int n = GetIndex(x, y);
+        if (n >= 0)
+        {
+            var cell = data.Substring(n, 1);
+
+            int c = cellValues.IndexOf(cell);
+            c++;
+            if (c >= cellValues.Count) { c = 0; }
+
+            cell = cellValues[c];
+
+        #if UNITY_EDITOR
+            Undo.RecordObject(this, "Toggle Cell");
+        #endif
+            // reassemble
+            data = data.Substring(0, n) + cell + data.Substring(n + 1);
+        #if UNITY_EDITOR
+            EditorUtility.SetDirty(this);
+        #endif
+        }
+    }
+
+    public bool IsValidCell(int x, int y)
+    {
+        return !(x < 0 || y < 0 || x >= width || y >= height);
+    }
+
+#if UNITY_EDITOR
+    public void OnValidate()
+    {
+        if (data == null || data.Length != (width * height))
+        {
+            Undo.RecordObject(this, "Resize");
+
+            if (width < 1) width = 1;
+            if (height < 1) height = 1;
+
+            // make a default layout
+            data = "";
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    string cell = "F";
+
+                    data = data + cell;
+                }
+            }
+
+            EditorUtility.SetDirty(this);
+        }
+    }
+
+    public void Reset()
+    {
+        OnValidate();
+    }
+#endif
+
+
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(LevelGrid))]
+    public class CheesyGridEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var grid = (LevelGrid)target;
+
+            EditorGUILayout.BeginVertical();
+
+            GUILayout.Label("WARNING: Save and commit your prefab/scene OFTEN!");
+            GUILayout.Label("F = NO ROOM");
+            GUILayout.Label("T = ROOM");
+            GUILayout.Label("* = (0, 0) ROOM");
+
+            for (int y = 0; y < grid.width; y++)
+            {
+                GUILayout.BeginHorizontal();
+                for (int x = 0; x < grid.height; x++)
+                {
+                    int n = grid.GetIndex(x, y);
+
+                    var cell = grid.data.Substring(n, 1);
+
+                    // hard-coded some cheesy color map - improve it by all means!
+                    GUI.color = Color.gray;
+                    if (cell == "F") GUI.color = Color.red;
+                    if (cell == "T") GUI.color = Color.white;
+                    if (cell == "*") GUI.color = Color.green;
+
+                    if (GUILayout.Button(cell, GUILayout.Width(20)))
+                    {
+                        grid.ToggleCell(x, y);
+                    }
+                }
+                GUILayout.EndHorizontal();
+            }
+            GUI.color = Color.yellow;
+
+            GUILayout.Label("DANGER ZONE BELOW THIS AREA!");
+
+            GUI.color = Color.white;
+
+            EditorGUILayout.EndVertical();
+
+            DrawDefaultInspector();
+        }
+    }
+#endif
+}
