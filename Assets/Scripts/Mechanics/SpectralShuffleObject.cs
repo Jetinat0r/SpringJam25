8a81e2ae876c5e0a68373d7613f516294c81e410
Jetinat0r <jetin8t0r@gmail.com>
2025-12-22 17:48:38 -0500

    Level 16 Spectral Shuffle

--- Assets/Scripts/Mechanics/SpectralShuffleObject.cs
+++ Assets/Scripts/Mechanics/SpectralShuffleObject.cs
@@ -6,7 +6,7 @@ public class SpectralShuffleObject : MonoBehaviour
     public bool destroyInSpectralShuffle = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         if (destroyInSpectralShuffle)
         {
