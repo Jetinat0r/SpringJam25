53c4b051f822b5fef6b4f3de86b15a7747867ab6
Jetinat0r <jetin8t0r@gmail.com>
2026-01-01 15:26:47 -0500

    Added Skip Button to Credits
    - Fixed Generic UI Interact Prompts

--- Assets/Scripts/UI/CreditsMenu.cs
+++ Assets/Scripts/UI/CreditsMenu.cs
@@ -1,7 +1,11 @@
 using DG.Tweening;
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -12,16 +16,96 @@ public class CreditsMenu : MonoBehaviour
     [SerializeField]
     public Button returnToMainMenuButton;
 
-    public SoundPlayable selectSound, challengesUnlockedSound, ditSound;
+    public SoundPlayable selectSound, challengesUnlockedSound, ditSound, backSound;
     public SoundPlayer soundPlayer;
     
     public GameObject popupBox;
     public SpriteRenderer fakeScreenWipe;
 
+    public GameObject skipBox;
+    //Lock to prevent the skip box from appearing & functioning after the credits have finished and before the scene has exited
+    public bool permanentSkipLock = false;
+    //Whether or not the skip box can currently be seen and is ready for the skip input
+    public bool skipBoxVisible = false;
+    //bool on a 1 frame delay to prevent insta skipping
+    public bool skipReady = false;
+    public float skipBoxDisappearTimeSeconds = 3f;
+    public Coroutine skipBoxDisappearRoutine = null;
+    IDisposable anyInputListener = null;
+
+    public PlayableDirector timeline;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //SetSelectedToReturnToMainMenuButton();
+        if (!ProgramManager.instance.showChallengeUnlock)
+        {
+            anyInputListener = InputSystem.onAnyButtonPress.Call((_eventPtr) => { TryShowSkipBox(); });
+            InputOverlord.instance.playerInput.actions["ToggleUI"].started += TrySkipCredits;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (!ProgramManager.instance.showChallengeUnlock)
+        {
+            anyInputListener.Dispose();
+            InputOverlord.instance.playerInput.actions["ToggleUI"].started -= TrySkipCredits;
+
+            if (skipBoxDisappearRoutine != null)
+            {
+                StopCoroutine(skipBoxDisappearRoutine);
+            }
+        }
+    }
+
+    private void TryShowSkipBox()
+    {
+        if (!permanentSkipLock)
+        {
+            skipBox.SetActive(true);
+            skipBoxVisible = true;
+            if (skipBoxDisappearRoutine != null)
+            {
+                StopCoroutine(skipBoxDisappearRoutine);
+            }
+            skipBoxDisappearRoutine = StartCoroutine(SkipBoxDisappearRoutine());
+            StartCoroutine(SkipEnable());
+        }
+    }
+
+    private IEnumerator SkipBoxDisappearRoutine()
+    {
+        yield return new WaitForSeconds(skipBoxDisappearTimeSeconds);
+        skipBox.SetActive(false);
+        skipBoxVisible = false;
+        skipBoxDisappearRoutine = null;
+        skipReady = false;
+    }
+
+    private IEnumerator SkipEnable()
+    {
+        yield return null;
+        skipReady = true;
+    }
+
+    private void TrySkipCredits(InputAction.CallbackContext _context)
+    {
+        if (!permanentSkipLock && skipBoxVisible && skipReady)
+        {
+            /*
+            StopCoroutine(skipBoxDisappearRoutine);
+            skipBoxDisappearRoutine = null;
+
+            permanentSkipLock = true;
+            */
+
+            soundPlayer.PlaySound(backSound);
+            timeline.Pause();
+
+            EndCreditsSequence();
+        }
     }
 
     //Called by end of credits sequence
@@ -53,6 +137,15 @@ public class CreditsMenu : MonoBehaviour
         }
         else
         {
+            skipReady = false;
+            if (skipBoxDisappearRoutine != null)
+            {
+                StopCoroutine(skipBoxDisappearRoutine);
+            }
+            skipBoxDisappearRoutine = null;
+
+            permanentSkipLock = true;
+
             ReturnToMainMenu();
         }
     }
