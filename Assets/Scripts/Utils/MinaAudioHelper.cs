3a444e51d44dece9134d51a6b38f4a0292425869
Evan McRae <evanmmcrae@gmail.com>
2026-06-07 00:15:37 -0400

    Add fall + landing sound events to timeline

---
+++ Assets/Scripts/Utils/MinaAudioHelper.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MinaAudioHelper : MonoBehaviour
+{
+    [SerializeField] private SoundPlayer soundPlayer;
+
+    public void PlayLandingSound()
+    {
+        soundPlayer.PlaySound("EasterEgg.Landing");
+    }
+
+    public void PlayFallSound()
+    {
+        soundPlayer.PlaySound("EasterEgg.Fall");
+    }
+
+    public void PlayJumpSound()
+    {
+        soundPlayer.PlaySound("EasterEgg.Jump");
+    }
+
+    public void PlayWhipSound()
+    {
+        soundPlayer.PlaySound("EasterEgg.Whip");
+    }
+
+    public void PlayTypewriterSound()
+    {
+        soundPlayer.PlaySound("EasterEgg.Typewriter");
+    }
+}
